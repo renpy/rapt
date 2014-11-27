8ae2f1d8d20fc181cdf7c006adbf8a16ca24b1c8
Tom Rothamel <tom@rothamel.us>
2014-11-26 23:33:19 -0500

    Upgrade color_touch to use APP events for app state.

--- tests/color_touch/main.py
+++ tests/color_touch/main.py
@@ -19,15 +19,12 @@ GREEN = (0, 255, 0, 255)
 
 def main():
     pygame.init()
+
     if android:
         android.init()
 
     # Set the screen size.
     screen = pygame.display.set_mode((480, 800))
-
-    # Map the back button to the escape key.
-    if android:
-        android.map_key(android.KEYCODE_BACK, pygame.K_ESCAPE)
 
     # Use a timer to control FPS.
     pygame.time.set_timer(TIMEREVENT, 1000 / FPS)
@@ -39,33 +36,38 @@ def main():
 
         ev = pygame.event.wait()
 
-        # Android-specific: 
-        if android:
-            if android.check_pause():
-                android.wait_for_resume()
+        # Handle application state events.
+        if ev.type == pygame.APP_WILLENTERBACKGROUND:
+            print "Will enter background."
+            pygame.time.set_timer(TIMEREVENT, 0)
+        elif ev.type == pygame.APP_DIDENTERFOREGROUND:
+            print "Did enter foreground."
+            screen = pygame.display.set_mode((480, 800))
+            pygame.time.set_timer(TIMEREVENT, 1000 / FPS)
+        elif ev.type == pygame.APP_TERMINATING:
+            break
 
         # Draw the screen based on the timer.
-        if ev.type == TIMEREVENT:
+        elif ev.type == TIMEREVENT:
             screen.fill(color)
             pygame.display.flip()
 
-        # When the touchscreen is pressed, change the color to green. 
+        # When the touchscreen is pressed, change the color to green.
         elif ev.type == pygame.MOUSEBUTTONDOWN:
             color = GREEN
             if android:
-                android.vibrate(.25)
-            
+                android.vibrate(.1)
+
         # When it's released, change the color to RED.
         elif ev.type == pygame.MOUSEBUTTONUP:
             color = RED
 
-        # When the user hits back, ESCAPE is sent. Handle it and end
-        # the game.
-        elif ev.type == pygame.KEYDOWN and ev.key == pygame.K_ESCAPE:
+        # Escape or the back button quits.
+        elif ev.type == pygame.KEYDOWN and (ev.key == pygame.K_ESCAPE or ev.key == pygame.K_AC_BACK):
             break
-    
+
 # This isn't run on Android.
 if __name__ == "__main__":
     main()
-    
-    
+
+
