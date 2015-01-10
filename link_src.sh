9537b5a97307ab4afe158f7e93c79af96ccd854e
Tom Rothamel <tom@rothamel.us>
2015-01-10 14:26:57 -0500

    Also link buildlib and templates.

--- link_src.sh
+++ link_src.sh
@@ -9,3 +9,8 @@ set -e
 rm -Rf "$dist/src"
 ln -s "$root/python-for-android/src/src" "$dist/src"
 
+rm -Rf "$dist/buildlib"
+ln -s "$root/buildlib" "$dist/buildlib"
+
+rm -Rf "$dist/templates"
+ln -s "$root/templates" "$dist/templates"
