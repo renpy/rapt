4b6d387f4ed8b5d86797d6e7db94c3c8a804873a
Tom Rothamel <tom@rothamel.us>
2016-05-25 22:52:43 -0400

    Fix build of python so we use the right version for the platform.

--- native/jni/python/Android.mk
+++ native/jni/python/Android.mk
@@ -3,8 +3,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := python
 
-LOCAL_SRC_FILES := $(INSTALLDIR)/lib/libpython2.7.so
-LOCAL_EXPORT_C_INCLUDES := $(INSTALLDIR)/include/python2.7
+LOCAL_EXPORT_C_INCLUDES := $(NATIVE)/install/$(TARGET_ARCH_ABI)/include/python2.7
+LOCAL_SRC_FILES := $(NATIVE)/install/$(TARGET_ARCH_ABI)/lib/libpython2.7.so
 
 include $(PREBUILT_SHARED_LIBRARY)
 
