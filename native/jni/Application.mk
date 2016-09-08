f858d3ffd64cf68a5f678c4d151b92859a1f07fc
Tom Rothamel <tom@rothamel.us>
2016-09-07 22:12:00 -0400

    Add webp support.

--- native/jni/Application.mk
+++ native/jni/Application.mk
@@ -1,6 +1,6 @@
 APP_PROJECT_PATH := $(call my-dir)/..
 
-APP_MODULES := python SDL2 png16 jpeg SDL2_image SDL2_gfx SDL2_ttf SDL2_mixer freetype main pymodules
+APP_MODULES := python SDL2 png16 jpeg libwebp SDL2_image SDL2_gfx SDL2_ttf SDL2_mixer freetype main pymodules
 
 APP_ABI := $(ARCH)
 APP_STL := gnustl_static
